36cd489627c37054a59d158a88e2fa257ef3fdc6
oriol.cuellar <oriol.cuellar@estudiantat.upc.edu>
2021-11-07 17:52:01 +0100

    casos d'us

--- controladores/CtrlDomini.java
+++ controladores/CtrlDomini.java
@@ -13,6 +13,18 @@ public class CtrlDomini {
     public static void getRecommendedItems(){}
     public static void getAllItems(){}
     public static void getUsedItems(){}
-    public static void login(){}
+    public static void selectItem(){}
+    public static void valoreItem(){}
+    public static void savee(){} //capa de dades??
+    public static void closee(){} // dan error
+    public static void createItem(){}
+    public static void deleteItem(){}
+    public static void modItem(){}
+    public static void loadItems(){}
+    public static void loadUsers(){}
+    public static void loadValorations(){}
+    public static void deleteUser(){}
+    public static void createUser(){}
+
 }
 
