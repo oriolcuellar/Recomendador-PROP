1c2f485d0c65e83bf85cc81f42f2a1f5883f7452
oriol <oriol.cuellar@estudiantat.upc>
2021-11-15 18:08:46 +0100

    ctrl

--- src/domini/model/RateRecomendation.java
+++ src/domini/model/RateRecomendation.java
@@ -1,14 +1,23 @@
 package src.domini.model;
 
 
+import java.util.ArrayList;
+
 public class RateRecomendation {
 
+    private static ArrayList<myPair> arr
 
-    public  RateRecomendation(){
-
-
+    public  RateRecomendation(ArrayList<myPair> arr2 ){
+        this.arr=arr2;
     }
     public void execute(){
-
+        float res=0;
+        for (int i=0;i<arr.size();++i){
+            double top=(Math.pow(2, arr.get(i).getValoration())-1);
+            double down=(Math.log10(i+1) / Math.log10(2));
+            top=top/down;
+            res+=top;
+        }
+        System.out.println(res);
     }
 }
