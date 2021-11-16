3123d6aaaf2177a02d63abec917760215320e0be
oriol.cuellar <oriol.cuellar@estudiantat.upc.edu>
2021-11-16 17:36:20 +0100

    controlador

--- src/domini/model/RateRecomendation.java
+++ src/domini/model/RateRecomendation.java
@@ -13,10 +13,16 @@ public class RateRecomendation {
     public void execute(){
         float res=0;
         for (int i=0;i<arr.size();++i){
+            //System.out.println(" ");
             double top=(Math.pow(2, arr.get(i).getValoration())-1);
+            //System.out.println(arr.get(i).getValoration());
             double down=(Math.log10(i+1+1) / Math.log10(2));
+
+            //System.out.println(top);
+            //System.out.println(down);
             top=top/down;
             res+=top;
+
         }
         System.out.println(res);
     }
