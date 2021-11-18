49c9406bb059f9dc67d88339d9998f475fd0fe72
oriol.cuellar <oriol.cuellar@estudiantat.upc.edu>
2021-11-18 17:37:10 +0100

    comentarios rate recom

--- FONTS/src/domini/Drivers/DriverLectorCSV2.java
+++ FONTS/src/domini/Drivers/DriverLectorCSV2.java
@@ -9,16 +9,29 @@ public class DriverLectorCSV2 {
 
 
     public static void main(String[] args) {
-        //Leer valoraciones
-        String path="Entradas_CSV/ratings.db.csv";
+
+        String path_Val="Entradas_CSV/ratings.db.csv";
+        String path_Items="Entradas_CSV/Items.csv";
 
         LectorCSV2 reader = new LectorCSV2();
+
+        //Leer valoraciones
         ArrayList<Vector<String>> readed_ratings = new ArrayList<Vector<String>>();
-        readed_ratings = reader.Lector_Ratings(path);
+        readed_ratings = reader.Lector_Ratings(path_Val);
 
         for(int i=0;i<readed_ratings.size();++i) {
             System.out.println(readed_ratings.get(i));
         }
+
+        //Leer Items
+        Vector<String> readed_Items = new Vector<String>();
+        readed_Items = reader.Lector_Items(path_Items);
+
+        for(int i=0;i<readed_Items.size();++i) {
+            System.out.println(readed_Items.get(i)+"\n");
+        }
+
+
 
     }
 }
