a808c930caedfdc51e76ae048cbbb9b2897c518f
marc.camarillas <marc.camarillas@estudiantat.upc.edu>
2021-12-21 13:46:15 +0100

    Capa Presentacion

--- FONTS/src/presentacion/AdminMainPage.java
+++ FONTS/src/presentacion/AdminMainPage.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/** \brief Clase que implementa la vista del menu principal del Administrador.
+ *  @author Marc Camarillas
+ */
 public class AdminMainPage {
     /**
      * Instancia del controlador de Presentacion
